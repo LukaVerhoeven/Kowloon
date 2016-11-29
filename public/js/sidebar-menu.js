61dbc5d8bb415c830b8e8da0a1e38f9afc65f998
LukaVerhoeven <lka.v.lv@gmail.com>
2016-11-29 14:24:18 +0100

    first commit

---
+++ public/js/sidebar-menu.js
@@ -0,0 +1,8 @@
+$('.menu').click(function () {
+if ($( ".full-sidebar" ).hasClass( "hidden" )) {
+  $('.full-sidebar').removeClass('hidden');
+}else {
+  $('.full-sidebar').addClass('hidden');
+}
+
+})
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="/css/main.css" />
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+      <div class="sidebar">
+        <ul>
+          <li><a href="#" class="menu"></a></li>
+          <li><a href="#" class="search"></a></li>
+          <li><a href="#" class="ask"></a></li>
+          <li class="line"></li>
+        </ul>
+        <ul class="catergory-menu">
+          <li><a href="#" class="dog"></a></li>
+          <li><a href="#" class="cat"></a></li>
+          <li><a href="#" class="fish"></a></li>
+          <li><a href="#" class="bird"></a></li>
+          <li><a href="#" class="rats"></a></li>
+        </ul>
+        <ul>
+          <li class="kowloon-menu"></li>
+        </ul>
+      </div>
+      <div class="full-sidebar hidden">
+        <div class="sidebar">
+          <ul>
+            <li><a href="#" class="menu"></a></li>
+            <li><a href="#" class="search"></a></li>
+            <li><a href="#" class="ask"></a></li>
+            <li class="line"></li>
+          </ul>
+          <ul class="catergory-menu">
+            <li><a href="#" class="dog"></a></li>
+            <li><a href="#" class="cat"></a></li>
+            <li><a href="#" class="fish"></a></li>
+            <li><a href="#" class="bird"></a></li>
+            <li><a href="#" class="rats"></a></li>
+          </ul>
+          <ul>
+            <li class="kowloon-menu full-kowloon"></li>
+          </ul>
+        </div>
+      </div>
+      <div class="header">
+        <img src="/img/kowloonLogo.png" alt="kowloon Logo">
+
+      </div>
+      <div class="content">
+        <div class="text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="icons">
+          <ul class="catergory-content">
+            <li><a href="#" class="dog-content"><img src="/img/dog.png" alt=""><p>DOGS</p></a><span class="cathegory-line"></span></li>
+            <li><a href="#" class="cat-content"><img src="/img/cat.png" alt=""><p>CATS</p></a><span class="cathegory-line"></span></li>
+            <li><a href="#" class="fish-content"><img src="/img/fish.png" alt=""><p>FISH</p></a><span class="cathegory-line"></span></li>
+            <li><a href="#" class="bird-content"><img src="/img/bird.png" alt=""><p>BIRDS</p></a><span class="cathegory-line"></span></li>
+            <li><a href="#" class="rat-content"><img src="/img/rat.png" alt=""><p class="small-p"><span class="small-text">SMALL</span> ANIMALS</p></a><span class="cathegory-line"></span></li>
+            <li><a href="#" class="other-content"><img src="/img/other.png" alt=""><p class="other-text">OTHER</p></a></li>
+
+          </ul>
+
+        </div>
+        <div class="hot-items">
+          <h1>HOT ITEMS.</h1>
+          <ul class="hot-item-list">
+            <li><img src="/img/dogmat.jpg" alt=""> <p>Cooling mat <span class="prijs">€ 15,49</span></p></li>
+            <li><img src="/img/dogmat.jpg" alt=""> <p>Cooling mat <span class="prijs">€ 15,49</span></p></li>
+            <li><img src="/img/dogmat.jpg" alt=""> <p>Cooling mat <span class="prijs">€ 15,49</span></p></li>
+            <li><img src="/img/dogmat.jpg" alt=""> <p>Cooling mat <span class="prijs">€ 15,49</span></p></li>
+          </ul>
+          <a href="#" class="visit">Visit the store</a>
+        </div>
+        <div class="newsletter">
+          <div class="news-img">
+            <h2>Discover amazing Knowloon deals!</h2>
+            <h4>only in our newsletter</h4>
+          </div>
+          <div class="news-form">
+            <h3>Subscribe to our newsletter</h3>
+            <p>Lorum ipsum dolor sit amet..</p>
+            <input type="text" name="" value="">
+            <input type="submit" name="" value="">
+          </div>
+
+        </div>
+      </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+      	<script src="/js/sidebar-menu.js"></script>
+    </body>
+</html>
